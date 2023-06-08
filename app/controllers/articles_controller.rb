54a3a95940c352729699b967670f3c198425a591
TommySM <tommythesmiley@gmail.com>
2023-06-08 12:45:15 +0200

    Users form and correct errors in articles form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,28 +18,34 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(article_params)
-        @article.user = User.first
+      @article = Article.new(article_params)
+    
+      respond_to do |format|
         if @article.save
-          flash[:notice] = "Article created successfully."
-          redirect_to @article
+          format.html { redirect_to @article, notice: 'Article was successfully created.' }
+          format.turbo_stream { redirect_to @article, notice: 'Article was successfully created.' }
         else
-          respond_to do |format|
-            format.html { render :new }
-            format.turbo_stream { render turbo_stream: turbo_stream.append("articles", partial: "shared/errors", locals: { errors: @article.errors.full_messages }) }
-          end
+          format.html { render :new }
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("errors", partial: "shared/errors", locals: { errors: @article.errors.full_messages, errors_record_name: "Article" }) }
         end
       end
+    end
     
     def update
+      respond_to do |format|
         if @article.update(article_params)
+          format.html do
             flash[:notice] = "Article was updated successfully"
             redirect_to @article
+          end
+          format.turbo_stream { redirect_to @article, notice: 'Article was successfully updated.' }
         else
-            render :edit
+          format.html { render :edit }
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("errors", partial: "shared/errors", locals: { errors: @article.errors.full_messages, errors_record_name: "Article" }) }
         end
+      end
     end
-
+    
     def destroy
         @article.destroy
         redirect_to articles_path        
