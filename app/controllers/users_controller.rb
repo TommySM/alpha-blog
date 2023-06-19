ca253f8284d407b62c4d289ae310d401754f270f
TommySM <tommythesmiley@gmail.com>
2023-06-19 17:05:39 +0200

    Add sessions and format code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   
   def show
-    @user = User.find(params[:id])
       @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         respond_to do |format|
             if @user.save
+              session[:user_id] = @user.id
               format.html do
                 flash[:notice] = "Welcome to the Alpha Blog, you have successfully signed up"
                 redirect_to articles_path
@@ -31,11 +32,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])            
     end
-    def update
-        @user = User.find(params[:id])
-        
+
+    def update        
         respond_to do |format|
           if @user.update(user_params)
             format.html do
@@ -54,4 +53,9 @@ class UsersController < ApplicationController
       def user_params
         params.require(:user).permit(:username, :email, :password, :profile_images)
       end
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
 end
