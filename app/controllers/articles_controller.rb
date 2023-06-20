36b5279fed6ea6b51f4f8df68aaebb59e0cc96b4
TommySM <tommythesmiley@gmail.com>
2023-06-20 03:34:43 +0200

    Add user restriction in article and navigation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
   include WillPaginate::ViewHelpers  
   include ActionView::RecordIdentifier
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index] 
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
     end
@@ -61,6 +63,14 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can only edit or delete your own article"
+        redirect_to @article        
+      end
+      
     end
 
 end
